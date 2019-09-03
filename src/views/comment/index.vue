2325dc7ab1ccb69fe66aa6a1f0f08e79c4650184
lxl <lxl@qq.com>
2019-09-03 21:27:00 +0800

    评论页表格生成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>评论列表</div>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">评论列表</template>
+      </bread-crumb>
+      <el-table :data="list">
+        <el-table-column width="700" label="标题" prop="data"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="总评论数" prop="name"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="address"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
