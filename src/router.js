5531e9337cd1e978ade3491089c60fad5b88367c
lxl <lxl@qq.com>
2019-09-07 08:34:44 +0800

    条件搜索-频道获取

--- src/router.js
+++ src/router.js
@@ -32,6 +32,9 @@ export default new Router({
       }, {
         path: 'articles',
         component: () => import('./views/articles/index.vue')
+      }, {
+        path: 'publish',
+        component: () => import('./views/publish/index.vue')
       }]
     }
     // {
