7b5f5f2490945b89c14c682adf92bd1a268d79ad
lxl <lxl@qq.com>
2019-09-09 20:02:11 +0800

    页面加载进度条

--- src/permission.js
+++ src/permission.js
@@ -1,5 +1,8 @@
 import router from './router'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  nprogress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -10,5 +13,8 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(function () {
+  nprogress.done()
 })
 export default router
